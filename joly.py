826f0a6872e5ebed806cfc6f75aff92698223cd6
premnathkulal <35401388+premnathkulal@users.noreply.github.com>
2019-05-16 18:49:24 +0530

    Update joly.py

--- joly.py
+++ joly.py
@@ -1,9 +1,69 @@
 
+
+def jolyJ(li1):
+    fflag = 0
+    fres = 0
+    rflag = 0
+    rres = 0
+
+    n = len(li)
+
+    t2 = 0
+
+    for i in range(0,n-1):
+
+        t = li1[i]
+        t3 = li1[i+1]
+        #print(t,t3)
+
+        if t > t3:
+            t1 = t - t3
+        else:
+            t1 = t3 - t
+
+        if i == 0:
+            t2 = t1
+            continue
+        else:
+            if i == 1:
+
+                if t2 == t1 + 1 and fflag == 0:
+                    fflag = 1
+
+                elif t2 == t1 - 1 and rflag == 0:
+                    rflag = 1
+
+            if t2 != t1+1 and fflag == 1:
+                fres = 1
+
+            elif t2 != t1-1 and rflag == 1:
+                rres = 1
+
+            t2 = t1
+
+    if fflag == 1 and fres == 0:
+        return  1
+    elif rflag == 1 and rres == 0:
+        return  1
+    else:
+        return 0
+
+
+
+
+
 print("Enter a number ")
 n = input()
+
 li = []
 
-for i in range(n.__size__()):
-	li[i] = int(n[i])
+for i in range(len(n)):
+    #print(n[i])
+    li.append(int(n[i]))
 
-print(li)
+res = jolyJ(li)
+
+if res == 1:
+    print("YES")
+else:
+    print("NO")
